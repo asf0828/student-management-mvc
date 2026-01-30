febcd50cbaa0cf32c19ffcba2cb4501efe8e4790
Alexander Sandoval <alexander.sf0828@gmail.com>
2026-01-29 21:49:09 -0500

    feat: add StudentRepository class for managing student collections
    Add StudentRepository class in model package with student list management
    Include methods to add students, retrieve all students, and find youngest/oldest students

---
+++ src/co/edu/umng/studentmanagement/model/StudentRepository.java
@@ -0,0 +1,42 @@
+package co.edu.umng.studentmanagement.model;
+
+public class StudentRepository {
+
+    private List<Student> students;
+    
+    public StudentRepository() {
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        this.students.add(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return this.students;
+    }
+
+    public Student getYoungestStudent() {
+        Student youngest = students.get(0);
+
+        for (Student student : students) {
+            if (student.getAge() < youngest.getAge()) {
+                youngest = student;
+            }
+        }
+
+        return youngest;
+    }
+
+    public Student getOldestStudent() {
+        Student oldest = students.get(0);
+
+        for (Student student : students) {
+            if (student.getAge() > oldest.getAge()) {
+                oldest = student;
+            }
+        }
+
+        return oldest;
+    }
+}
