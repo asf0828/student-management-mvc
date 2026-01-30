02f751c6de5a011fb8369d8ccc7a596802737868
Alexander Sandoval <alexander.sf0828@gmail.com>
2026-01-29 21:52:23 -0500

    feat: add StudentView class for displaying student information
    Add StudentView class in view package with methods to display student lists and individual students
    Include methods to show youngest/oldest students and format student data for output

---
+++ src/co/edu/umng/studentmanagement/view/StudentView.java
@@ -0,0 +1,28 @@
+package co.edu.umng.studentmanagement.view;
+
+public class StudentView {
+
+    public void showStudentList(List<Student> students) {
+        System.out.println("Información de estudiantes:");
+        for (Student student : students) {
+            System.out.println(formatStudent(student));
+        }
+    }
+
+    public void showYoungestStudent(Student student) {
+        System.out.println("Estudiante con la Menor Edad:");
+        System.out.println(formatStudent(student));
+        System.out.println();
+    }
+
+    public void showOldestStudent(Student student) {
+        System.out.println("Estudiante con la Mayor Edad:");
+        System.out.println(formatStudent(student));
+        System.out.println();
+    }
+
+    private String formatStudent(Student student) {
+        return student.getName() + ", " + student.getAge() + " años, " + student.getSemester() + " semestre";
+    }
+}
+
